bf272d14ee5f0f4655030ccbe49ef42f614309a7
Adson Martins <adson.martins982@gmail.com>
2023-04-29 17:41:45 -0300

    initial state project

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  distDir: "dist",
   experimental: {
     appDir: true,
   },
-}
+  env: {
+    isDev: process.env.NODE_ENV !== "production",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
