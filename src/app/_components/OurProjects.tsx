5b113bc3a91dfa672f6ac15abde8f2c285779e60
Adson Martins <adson.martins982@gmail.com>
2023-04-30 05:18:34 -0300

    started projects section

---
+++ src/app/_components/OurProjects.tsx
@@ -0,0 +1,9 @@
+import { SectionTitle } from './SectionTitle'
+
+export function OurProjects() {
+  return (
+    <section className="mt-20 lg:mt-24">
+      <SectionTitle className="lg:!w-6/12" title="Conheça nossos projetos" />
+    </section>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { SocialMedia } from '@/components/SocialMedia'
 import Image from 'next/image'
 import { GoBackButton } from './_components/GoBackButton'
 import { OurMission } from './_components/OurMission'
+import { OurProjects } from './_components/OurProjects'
 import { OurTeam } from './_components/OurTeam'
 import { WhatIsSpaceship } from './_components/WhatIsSpaceship'
 
@@ -21,6 +22,7 @@ export default function Home() {
         <WhatIsSpaceship />
         <OurMission />
         <OurTeam />
+        <OurProjects />
       </main>
       <footer className="mt-24 bg-[#15191F] p-8 lg:mt-[100px]">
         <div className="container mx-auto flex flex-col items-center justify-center lg:flex-row">
